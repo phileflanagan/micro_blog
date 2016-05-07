4ebb5523c6ca9284886d25fd07c82cf23f77d4ec
Phil Flanagan <phileflanagan@gmail.com>
2016-05-07 00:07:05 +0000

    Finish refactoring code for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -8,11 +10,10 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    find_article
   end
 
   def create
-    find_article
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created."
       redirect_to article_path(@article)
@@ -22,7 +23,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    find_article
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
       redirect_to article_path(@article)
@@ -32,11 +32,9 @@ class ArticlesController < ApplicationController
   end
   
   def show 
-    find_article
   end
   
   def destroy
-    find_article
     @article.destroy
     flash[:notice] = "Article was successfully deleted."
     redirect_to articles_path 
